88b4ddb9645dc7c277c585cdfc2cbd4a252e983d
SophiMary <sofy.mary@outlook.com>
2020-10-29 02:31:09 +0530

    add sample Pricing Web App

--- index.js
+++ index.js
@@ -1,13 +1,15 @@
 const scriptURL =
 	"https://script.google.com/macros/s/AKfycbxVCHnC2buvjNzI9JlmutlCPlj-ZnrzYKynL1xQhUW5h2TcJkY/exec";
-const form = document.forms["submit-to-google-sheet"];function handleClick() {
-    document.querySelector(".row").classList.remove("display");
+const form = document.forms["submit-to-google-sheet"];
+
+function handleClick() {
+	document.querySelector(".row").classList.remove("display");
 }
 
 function openForm() {
-    document.querySelector(".form-container").classList.remove("display");
-    document.querySelector(".row").className = "display"
-    document.querySelector(".back").classList.remove("display");
+	document.querySelector(".form-container").classList.remove("display");
+	document.querySelector(".row").className = "display";
+	document.querySelector(".back").classList.remove("display");
 }
 
 form.addEventListener("submit", (e) => {
@@ -16,10 +18,10 @@ form.addEventListener("submit", (e) => {
 		method: "POST",
 		body: new FormData(form),
 	})
-        .then((response) => {
-            console.log("Success!", response.status);
-            alert("Data Successfully Submitted");
-            window.location.href="/index.html";
-        })
+		.then((response) => {
+			console.log("Success!", response.status);
+			alert("Data Successfully Submitted");
+			window.location.href = "/index.html";
+		})
 		.catch((error) => console.error("Error!", error.message));
 });
